968b056b4c4d54248a9bd85465160ce7984b9097
timgoodman047 <tim.goodman047@gmail.com>
2026-09-23 15:41:47 -0500

    Fix HTML string formatting in app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 document.getElementById("standings").innerHTML =
-    "<div class='team'>✅ JavaScript Loaded</div>";
+"<div class='team'>✅ JavaScript Loaded</div>";
  
 document.getElementById("powerRankings").innerHTML =
-    "<div class='team'>✅ Rankings Loaded</div>";
+"<div class='team'>✅ Rankings Loaded</div>";
  
 document.getElementById("dressTracker").innerHTML =
-    "<div class='team'>✅ Dress Tracker Loaded</div>";
+"<div class='team'>✅ Dress Tracker Loaded</div>";
  
 document.getElementById("news").innerHTML =
-    "✅ app.js is running";
+"✅ app.js is running";
